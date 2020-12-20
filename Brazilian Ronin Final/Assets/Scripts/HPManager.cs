53e7275e3965ebda3d3c0778bb02c127cda33e16
Chezzyz <48602862+Chezzyz@users.noreply.github.com>
2020-12-20 22:39:00 +0500

    ХП менеджер

--- Brazilian Ronin Final/Assets/Scripts/HPManager.cs
+++ Brazilian Ronin Final/Assets/Scripts/HPManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HPMangaer : MonoBehaviour
+public class HPManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int HPCount { get; private set; }
+    public static int HPmax = 3;
+    public Image hp1;
+    public Image hp2;
+    public Image hp3;
+    public Image[] images;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        HPCount = HPmax;
+        Enemy.MakeDamage += TakeDamage;
+        images = new Image[] { hp1,hp2,hp3 };
+    }
+    private void TakeDamage(int num)
+    {
+        HPCount -= num;
+        if (HPCount < images.Length)
+        {
+            images[HPCount].enabled = false;
+        }
     }
 }
